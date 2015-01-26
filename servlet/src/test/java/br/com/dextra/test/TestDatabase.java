a2cab193d87e9be5e85837bff838e6db8f56145e
joao vendrame <joao.vendrame@dextra-sw.com>
2015-01-26 09:26:34 -0200

    Novo: Junit teste para metodo gerarJsonNoticias

--- servlet/src/test/java/br/com/dextra/test/TestDatabase.java
+++ servlet/src/test/java/br/com/dextra/test/TestDatabase.java
@@ -11,6 +11,8 @@ import org.junit.Test;
 
 import br.com.dextra.database.AssuntoDao;
 import br.com.dextra.database.PerfilDao;
+import br.com.dextra.database.Noticia;
+import br.com.dextra.database.Gerenciadora;
 
 public class TestDatabase {
 	
@@ -42,6 +44,23 @@ public class TestDatabase {
 	
 	@Test
 	public void testGerarJsonNoticias() {
+		
+		Noticia n = new Noticia(1, "Teste 1","teste 1");
+		Noticia n2 = new Noticia(1, "Teste 2","teste 2");
+		Noticia n3 = new Noticia(1, "Teste 3","teste 3");
+
+		List<Noticia> noticias = new ArrayList<Noticia>();
+		noticias.add(n);
+		noticias.add(n2);
+		noticias.add(n3);
+		
+		Gerenciadora geren = new Gerenciadora();
+		String retorno = geren.gerarJsonDeNoticias(noticias);
+		String esperado = ""; 
+		
+		Assert.assertNotEquals(retorno,esperado);
+		
+		
 		
 	}
 
